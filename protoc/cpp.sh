d5ada7277cf0600a88b9ab2ef8ba22f251918a51
leehoon95 <leehoon95@naver.com>
2025-08-05 22:06:42 +0900

    cmake, gitignore 파일 수정

--- protoc/cpp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-protoc --cpp_out=. proto_message.proto
-
-protoc --cpp_out=. error_message.proto
-protoc --cpp_out=. login_message.proto
-protoc --cpp_out=. room_message.proto
-protoc --cpp_out=. game_message.proto
-protoc --cpp_out=. sync_message.proto
