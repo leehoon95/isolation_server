3e1b39b23e969c4a5bfdedcb6615472db9c0bf03
leehoon95 <leehoon95@naver.com>
2026-08-02 22:26:15 +0900

    fix: gtest 결과 파일 경로 수정

--- runtest.sh
+++ runtest.sh
@@ -2,4 +2,4 @@ cd tests
 cmake -B build \
     -DCMAKE_BUILD_TYPE=Debug \
     && cmake --build build -j${nproc} \
-    && build/run_unit_tests --output-on-failure
+    && build/run_unit_tests --output-on-failure --gtest_output=xml:build/gtest_results.xml
